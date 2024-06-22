7e503286b7ed473096adba6c75f0b55922eed59a
ISongwut <isongwut.me@gmail.com>
2024-06-22 20:06:09 +0700

    auth-service:chore: change go.mod & go.sum

--- services/auth-service/app/main.go
+++ services/auth-service/app/main.go
@@ -1,1 +1,27 @@
 package main
+
+import (
+	"log"
+	"os"
+
+	"github.com/joho/godotenv"
+	"github.com/xams-backend/services/auth-service/src/packages/database"
+)
+
+func main() {
+	godotenv.Load() //Load all environments
+
+	db := database.NewDatabase()
+	_, err := db.Connect()
+	if err != nil {
+		log.Fatalf("Failed to connect to database: %v", err)
+	}
+
+	if mode := os.Getenv("MODE"); mode == "production" {
+		err = db.AutoMigration()
+		if err != nil {
+			log.Fatalf("Failed to migrate database: %v", err)
+		}
+	}
+	
+}
