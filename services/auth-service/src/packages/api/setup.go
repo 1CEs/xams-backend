ed65bc28ff79687d5cb0dbf9e93c627f649079a3
ISongwut <isongwut.me@gmail.com>
2024-06-23 22:20:52 +0700

    auth-service:add: setup routes

---
+++ services/auth-service/src/packages/api/setup.go
@@ -0,0 +1,19 @@
+package api
+
+import (
+	"github.com/gin-gonic/gin"
+	"github.com/xams-backend/services/auth-service/src/packages/api/routes"
+	"gorm.io/gorm"
+)
+
+func SetupRoutes(db *gorm.DB, gin *gin.Engine) {
+	// For authentication route
+	route := gin.Group("/auth")
+	routes.LoginRoute(db, route)
+	routes.RegisterRoute(db, route)
+
+	// For user route
+	route = gin.Group("")
+	routes.UserRoute(db, route)
+
+}
