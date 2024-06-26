5df61aac3d3371ca01aa15035264a0f812e8ac3a
ISongwut <isongwut.me@gmail.com>
2024-06-26 19:52:24 +0700

    auth-service:add: apply simple middleware with user route

--- services/auth-service/src/packages/api/setup.go
+++ services/auth-service/src/packages/api/setup.go
@@ -2,6 +2,7 @@ package api
 
 import (
 	"github.com/gin-gonic/gin"
+	"github.com/xams-backend/services/auth-service/src/packages/api/middleware"
 	"github.com/xams-backend/services/auth-service/src/packages/api/routes"
 	"gorm.io/gorm"
 )
@@ -13,7 +14,10 @@ func SetupRoutes(db *gorm.DB, gin *gin.Engine) {
 	routes.RegisterRoute(db, route)
 
 	// For user route
+	middleware := middleware.Middleware{}
+
 	route = gin.Group("")
+	route.Use(middleware.IsAuthorized)
 	routes.UserRoute(db, route)
 
 }
