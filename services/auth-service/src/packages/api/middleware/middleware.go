8481d3b7e268dccc7075e58d6e085ca0c811b8f3
ISongwut <isongwut.me@gmail.com>
2024-06-26 19:51:16 +0700

    auth-service:add: implementing authorized checker & add parsedJWT utils

--- services/auth-service/src/packages/api/middleware/middleware.go
+++ services/auth-service/src/packages/api/middleware/middleware.go
@@ -1,9 +1,31 @@
 package middleware
 
-import "github.com/gin-gonic/gin"
+import (
+	"log"
+	"net/http"
+	"os"
+
+	"github.com/gin-gonic/gin"
+	"github.com/xams-backend/services/auth-service/src/packages/utils/middleware"
+)
 
 type Middleware struct{}
 
 func (m *Middleware) IsAuthorized(context *gin.Context) {
-	
+	cookieName := os.Getenv("COOKIE_JWT_TOKEN")
+	token, err := context.Cookie(cookieName)
+	if err != nil {
+		context.JSON(http.StatusUnauthorized, gin.H{"message": "Not authorized"})
+		return
+	}
+
+	mu := middleware.MiddlewareUtils{}
+	claims, err := mu.ParsedJWT(token)
+	if err != nil {
+		context.JSON(http.StatusInternalServerError, gin.H{"message": err})
+	}
+
+	log.Println(claims)
+
+	context.Next()
 }
---
+++ services/auth-service/src/packages/utils/middleware/utils.go
@@ -0,0 +1,35 @@
+package middleware
+
+import (
+	"errors"
+	"os"
+
+	"github.com/golang-jwt/jwt/v4"
+)
+
+type MiddlewareUtils struct{}
+
+func (mu *MiddlewareUtils) ParsedJWT(tokenString string) (jwt.MapClaims, error) {
+	claims := jwt.MapClaims{}
+	jwtSecret := os.Getenv("JWT_SECRET")
+	if jwtSecret == "" {
+		return nil, errors.New("JWT_SECRET environment variable not set")
+	}
+
+	token, err := jwt.ParseWithClaims(tokenString, claims, func(token *jwt.Token) (interface{}, error) {
+		if _, ok := token.Method.(*jwt.SigningMethodHMAC); !ok {
+			return nil, errors.New("unexpected signing method")
+		}
+		return []byte(jwtSecret), nil
+	})
+
+	if err != nil {
+		return nil, err
+	}
+
+	if !token.Valid {
+		return nil, errors.New("invalid token")
+	}
+
+	return claims, nil
+}
