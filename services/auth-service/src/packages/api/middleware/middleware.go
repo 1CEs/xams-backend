3c7c5c4335ecbb40f0c016900bbcfcfc91c33d53
ISongwut <isongwut.me@gmail.com>
2024-06-27 19:26:13 +0700

    auth-service:finish: Authorization middleware using credentials from jwt

--- services/auth-service/src/packages/api/middleware/middleware.go
+++ services/auth-service/src/packages/api/middleware/middleware.go
@@ -6,6 +6,9 @@ import (
 	"os"
 
 	"github.com/gin-gonic/gin"
+	"github.com/xams-backend/services/auth-service/src/internal/users/repository"
+	"github.com/xams-backend/services/auth-service/src/internal/users/usecase"
+	"github.com/xams-backend/services/auth-service/src/packages/database"
 	"github.com/xams-backend/services/auth-service/src/packages/utils/middleware"
 )
 
@@ -14,18 +17,38 @@ type Middleware struct{}
 func (m *Middleware) IsAuthorized(context *gin.Context) {
 	cookieName := os.Getenv("COOKIE_JWT_TOKEN")
 	token, err := context.Cookie(cookieName)
+
 	if err != nil {
 		context.JSON(http.StatusUnauthorized, gin.H{"message": "Not authorized"})
+		context.Abort()
 		return
 	}
 
 	mu := middleware.MiddlewareUtils{}
 	claims, err := mu.ParsedJWT(token)
 	if err != nil {
-		context.JSON(http.StatusInternalServerError, gin.H{"message": err})
+		context.JSON(http.StatusUnauthorized, gin.H{"message": err.Error()})
+		context.Abort()
+		return
+	}
+	log.Println(claims.UserID)	
+	db := database.NewDatabase()
+	connect, err := db.Connect()
+
+	if err != nil {
+		context.JSON(http.StatusInternalServerError, gin.H{"message": err.Error()})
+		context.Abort()
+		return
 	}
 
-	log.Println(claims)
+	ur := repository.NewUserRepository(connect)
+	uc := usecase.NewUserUsecase(ur)
 
+	if err := uc.IsUserAlreadyExists(claims.UserID); err != nil {
+		context.JSON(http.StatusUnauthorized, gin.H{"message": "Your credentials is not in our service."})
+		context.Abort()
+		return
+	}
+	
 	context.Next()
 }
